966ccb5d8e53b791bac74ed51870485820339c56
Jing Zhao <zhaojinghua@hotmail.com>
2024-01-04 09:55:19 -0800

    Responsive trip row working

--- src/app/trip-table/daily-trips/trip/trip.component.ts
+++ src/app/trip-table/daily-trips/trip/trip.component.ts
@@ -1,11 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Trip} from "./trip.model";
 
 @Component({
   selector: 'app-trip',
   templateUrl: './trip.component.html',
   styleUrls: ['./trip.component.css']
 })
-export class TripComponent {
+export class TripComponent implements OnInit{
 
+  trip: Trip = new Trip('123', 'New Huck Finn', 25, 25, 200, new Date('2024-01-08T06:00:00'), new Date('2024-01-08T15:00:00'), "Chef on board. Bring $25 cash for breakfast and lunch!!");
 
+  ngOnInit() {
+  }
 }

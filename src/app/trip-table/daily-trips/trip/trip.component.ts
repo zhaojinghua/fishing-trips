9e3069865760b0ff03ab1b4d9ea03e57ae8f792a
Jing Zhao <zhaojinghua@hotmail.com>
2024-01-06 13:37:46 -0800

    Better trip table

--- src/app/trip-table/daily-trips/trip/trip.component.ts
+++ src/app/trip-table/daily-trips/trip/trip.component.ts
@@ -10,6 +10,8 @@ export class TripComponent implements OnInit {
 
   // trip: Trip = new Trip('123', 'New Huck Finn', 25, 25, 200, new Date('2024-01-08T06:00:00'), new Date('2024-01-08T15:00:00'), "Chef on board. Bring $25 cash for breakfast and lunch!!");
   @Input() trip: Trip;
+  @Input() isFirstChild: boolean;
+  @Input() isLastChild: boolean;
 
   ngOnInit() {
   }

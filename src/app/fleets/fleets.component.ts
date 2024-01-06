3833ad6363c4e74037d35cfa478f1e3abf34a2e6
Jing Zhao <zhaojinghua@hotmail.com>
2024-01-05 19:56:19 -0800

    fleet cards and google map working

--- src/app/fleets/fleets.component.ts
+++ src/app/fleets/fleets.component.ts
@@ -8,9 +8,9 @@ import {Boat} from "./boat.model";
 })
 export class FleetsComponent {
 
-  boats: Boat[] = [new Boat("https://www.fishemeryville.com/graphics/boats/nhf_large.jpg", "Jay Yokomizo", "New Huck Finn"),
-    new Boat("https://www.fishemeryville.com/graphics/boats/SW_new_large.jpg", "Jon Yokomizo", "Sea Wolf"),
-    new Boat("https://www.fishemeryville.com/graphics/boats/CGull2_large.jpg", "Don Wong", "C-Gull II"),
-    new Boat("https://www.fishemeryville.com/graphics/boats/pp_2022__1__large.jpg", "JJ Holloway", "Pacific Pearl")
+  boats: Boat[] = [new Boat("/assets/newhuckfinn.jpg", "Jay Yokomizo", "New Huck Finn"),
+    new Boat("/assets/seawolf.jpg", "Jon Yokomizo", "Sea Wolf"),
+    new Boat("/assets/cgullii.jpg", "Don Wong", "C-Gull II"),
+    new Boat("/assets/pacificpearl.jpg", "JJ Holloway", "Pacific Pearl")
   ]
 }
